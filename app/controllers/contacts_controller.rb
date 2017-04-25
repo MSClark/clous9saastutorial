5034023d665f2a63b6b2f4abf10e863c743b566c
Matthew Clark <msclark3@asu.edu>
2017-04-25 18:22:23 +0000

    added Hirb to make rails console data presentation look better

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,19 @@
 class ContactsController < ApplicationController
   def new
-  @contact = Contact.new
+   @contact = Contact.new
   end
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+#bring them back to the blank contact page with message sent or error
        redirect_to new_contact_path, notice: "Message sent."
     else
        redirect_to new_contact_path, notice: "Error occured."
     end
   end
+# White list's params as a security feature using private keyword in order to mass assign
   private
     def contact_params
-       params.require(:contact).permit(:name, :email, :comments)
+       params.require(:contact).permit(:name, :email, :comments) #mass assign: assign multiple things at once
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,3 @@
 class Contact < ActiveRecord::Base
-    
+   # sort of blueprint for the class, but doesnt have anything yet because ruby goes the the DB and sees the object attributes for a contact  
 end
