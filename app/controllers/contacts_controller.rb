500dddc012e1c46ce79ac9a5365ed237df3fd114
Matthew Clark <msclark3@asu.edu>
2017-04-30 20:27:33 +0000

    commented contact files

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,18 +5,24 @@ class ContactsController < ApplicationController
    #then goes to 'new' view
   end
   def create
+    #mass assignment of form fields into contact object
     @contact = Contact.new(contact_params)
+    #save contact object to db
     if @contact.save
+      #store form fields via params to vars
       name=params[:contact][:name] #grabs name email and comments from form
       #params is a hash (key value pair)[] notation grabs what you want
       #nested hash example- params= {contact:{name:"joe", email:example@aol.com}}
       email=params[:contact][:email]
       body=params[:contact][:comments]
+      #plug vars into contact mailer email method and send email
       ContactMailer.contact_email(name, email, body).deliver #sends email
       #bring them back to the blank contact page with message sent or error
+      #store success message in flash hash and redirect to new page
       flash[:success]="Message sent"
       edirect_to new_contact_path
     else
+      #if contact doesnt save, store errors in flash hash and redirect to new page
        flash[:danger] = @contact.errors.full_messages.join(", ") 
        #join omits "" and the [] then delimits with ' and space
        redirect_to new_contact_path
