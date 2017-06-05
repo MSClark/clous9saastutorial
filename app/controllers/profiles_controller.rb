2b06566a30819adc81b7d2b4354f0c64d56d89a2
Matthew Clark <msclark3@asu.edu>
2017-06-05 00:58:38 +0000

    Added functionality to add profile pictures

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -28,6 +28,6 @@ class ProfilesController < ApplicationController
 private
   #whitelisting form params for security
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
   end
 end
